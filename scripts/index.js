6d5e23f4e081a95b7dac609c39bb3c2faa19dd79
Olga Yagnyuk <olga07122007@yandex.ru>
2021-11-13 19:07:03 +0300

    fix:исправлены ошибки 1

--- scripts/index.js
+++ scripts/index.js
@@ -35,26 +35,26 @@ const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
 //для редактирования профиля
-const editProfile = document.querySelector('.profile__edit-button');
+const buttonEditOpen = document.querySelector('.profile__edit-button');
 const popupEdit = document.querySelector('.popup_type_edit');
-const closeEditPopup = document.querySelector('.popup_type_edit .popup__close-icon');
+const buttonEditClose = document.querySelector('.popup_type_edit .popup__close-icon');
 
-const editFormElement = document.querySelector('.popup_type_edit .popup__input');
-const nameInput = editFormElement.querySelector('.popup_type_edit .popup__text_type_name');
-const jobInput = editFormElement.querySelector('.popup_type_edit .popup__text_type_about');
+const formEdit = document.querySelector('.popup_type_edit .popup__input');
+const nameInput = formEdit.querySelector('.popup_type_edit .popup__text_type_name');
+const jobInput = formEdit.querySelector('.popup_type_edit .popup__text_type_about');
 
 //для добавления фото
-const addPhoto = document.querySelector('.profile__add-button');
+const buttonAddOpen = document.querySelector('.profile__add-button');
 const popupAdd = document.querySelector('.popup_type_add');
-const closeAddPopup = document.querySelector('.popup_type_add .popup__close-icon'); 
+const buttoAddClose = document.querySelector('.popup_type_add .popup__close-icon'); 
 
-const addFormElement = document.querySelector('.popup_type_add .popup__input');
-const photoNameInput = addFormElement.querySelector('.popup_type_add .popup__text_type_name');
-const photoSrcInput = addFormElement.querySelector('.popup_type_add .popup__text_type_about');
+const formAdd = document.querySelector('.popup_type_add .popup__input');
+const photoNameInput = formAdd.querySelector('.popup_type_add .popup__text_type_name');
+const photoSrcInput = formAdd.querySelector('.popup_type_add .popup__text_type_about');
 
 //для увеличения фото
 const popupImage = document.querySelector('.popup_type_image');
-const closeImagePopup = document.querySelector('.popup_type_image .popup__close-icon');
+const buttonImageClose = document.querySelector('.popup_type_image .popup__close-icon');
 const popupImageImg = popupImage.querySelector('.popup__image'); 
 const popupImageTitle = popupImage.querySelector('.popup__description');
 
@@ -69,37 +69,34 @@ function closeForm(popupElement) {
 }
 
 //функция редактирования профиля
-function formSubmitHandler(evt) {
+function editProfile(evt) {
 	evt.preventDefault();
 	profileTitle.textContent=nameInput.value;
 	profileSubtitle.textContent=jobInput.value;
-	popupEdit.classList.remove('popup_opened');
+	closeForm(popupEdit);
 }
 
 //ФУНКЦИЯ - создание элемента
-function elementCreate (Name, Link) {
-	const userElement = userTemplate.querySelector('.element').cloneNode(true); 
-	userElement.querySelector('.element__image').src = Link; 
-	userElement.querySelector('.element__title').textContent = Name;
-	userElement.querySelector('.element__image').alt = Name;
+function createNewElement (newName, newLink) {
+	const userElement = userTemplate.querySelector('.element').cloneNode(true);
+	const elementImage = userElement.querySelector('.element__image');
+	elementImage.src = newLink; 
+	userElement.querySelector('.element__title').textContent = newName;
+	elementImage.alt = newName;
 	//сердечко
 	userElement.querySelector('.element__like').addEventListener('click', evt => {
-		const activeLike = evt.target;
-		activeLike.classList.toggle('element__like_active');
+		const buttonLike = evt.target;
+		buttonLike.classList.toggle('element__like_active');
 	});
 	//удаление
-	userElement.querySelector('.element__delete').addEventListener('click', evt => {
-		const deleteButton = evt.target;
-		const deleteElement = deleteButton.closest('.element');
-		deleteElement.remove();
+	userElement.querySelector('.element__delete').addEventListener('click', () => {
+		userElement.remove();
 	});
 	//открытие попапа с картинкой
-	userElement.querySelector('.element__image').addEventListener('click', evt => {
-		const elementImage = evt.target;
+	elementImage.addEventListener('click', () => {
 		openForm(popupImage);
-		popupImageImg.src = elementImage.src;
-		const elementOpen = elementImage.closest('.element');
-		popupImageTitle.textContent = elementOpen.textContent;
+		popupImageImg.src = newLink;
+		popupImageTitle.textContent = newName;
 	});
 	return userElement;
 }
@@ -107,43 +104,43 @@ function elementCreate (Name, Link) {
 //функция добавления фото
 function addPhotoElement (evt) {
 	evt.preventDefault();
-	sectionElements.prepend(elementCreate (photoNameInput.value, photoSrcInput.value));
-	popupAdd.classList.remove('popup_opened');
+	sectionElements.prepend(createNewElement (photoNameInput.value, photoSrcInput.value));
+	closeForm(popupAdd);
 }
 
 //открытие попапа
-editProfile.addEventListener('click', () => {
+buttonEditOpen.addEventListener('click', () => {
   openForm(popupEdit);
 	nameInput.value=profileTitle.textContent;
 	jobInput.value=profileSubtitle.textContent;
 });
 
-addPhoto.addEventListener('click', () => {
+buttonAddOpen.addEventListener('click', () => {
   openForm(popupAdd);
 	photoNameInput.value = '';
 	photoSrcInput.value = '';
 });
 
 //закрытие попапа
-closeEditPopup.addEventListener('click', () => {
+buttonEditClose.addEventListener('click', () => {
   closeForm(popupEdit);
 });
 
-closeAddPopup.addEventListener('click', () => {
+buttoAddClose.addEventListener('click', () => {
   closeForm(popupAdd);
 });
 
-closeImagePopup.addEventListener('click', () => {
+buttonImageClose.addEventListener('click', () => {
   closeForm(popupImage);
 });
 
 //редактирование профиля
-editFormElement.addEventListener('submit', formSubmitHandler);
+formEdit.addEventListener('submit', editProfile);
 
 //добавление фото
-addFormElement.addEventListener('submit', addPhotoElement);
+formAdd.addEventListener('submit', addPhotoElement);
 
 //добавление массива элементов при загрузке страницы
-initialCards.forEach((card,index) => {
-	sectionElements.append(elementCreate (initialCards[index].name, initialCards[index].link));
+initialCards.forEach(card => {
+	sectionElements.append(createNewElement (card.name, card.link));
 });
